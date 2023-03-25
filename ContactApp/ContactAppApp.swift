c9a1555a0d09b02492e1bc57334f641c18285431
Mert Urhan <mert-urhan@hotmail.com>
2023-03-25 21:13:45 +0300

    Initial Commit

---
+++ ContactApp/ContactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactAppApp.swift
+//  ContactApp
+//
+//  Created by Mert Urhan on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
